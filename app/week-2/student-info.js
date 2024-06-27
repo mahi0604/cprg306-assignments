254ebb28fccf319ac86043f1397ef6b3e38a5415
mahi0604 <141184262+mahi0604@users.noreply.github.com>
2024-06-27 17:58:34 -0600

    Update student-info.js
    week-2task-2

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,1 +1,13 @@
+import Link from 'next/link';
+
+const StudentInfo = () => (
+  <div>
+    <h1>Mahi Vipul Patel</h1>
+    <Link href="https://github.com/mahi0604">
+      <a target="_blank">GitHub Repository</a>
+    </Link>
+  </div>
+);
+
+export default StudentInfo;
 
