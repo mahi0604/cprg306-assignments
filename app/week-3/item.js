a3213cdbec59d4a8bf2d9d980a79f892c5a3dbf2
mahi0604 <141184262+mahi0604@users.noreply.github.com>
2024-06-27 18:25:43 -0600

    Update item.js
    week-3task-2

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+const Item = ({ name, quantity, category }) => {
+  return (
+    <li className="p-2 border-b border-gray-200">
+      <div className="font-bold">{name}</div>
+      <div className="text-gray-600">{quantity} ({category})</div>
+    </li>
+  );
+};
+
+export default Item;
 
