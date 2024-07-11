724b3d17a47ec1a464f17d52f11226bffbcb540c
mahi0604 <141184262+mahi0604@users.noreply.github.com>
2024-07-11 11:53:49 -0600

    Update page.js
    week4part2task1

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import NewItem from './new-item';
 
+const Page = () => {
+  return (
+    <main className="p-4 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold mb-6 text-center">Week 4 Assignment</h1>
+      <NewItem />
+    </main>
+  );
+};
+
+export default Page;
