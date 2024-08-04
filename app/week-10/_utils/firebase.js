1a78c642c2288e8da57c59e16bac221f0d9bac0c
mahi0604 <141184262+mahi0604@users.noreply.github.com>
2024-08-04 14:57:14 -0600

    Update firebase.js

--- app/week-10/_utils/firebase.js
+++ app/week-10/_utils/firebase.js
@@ -1,6 +1,8 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,5 +13,10 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+
 const app = initializeApp(firebaseConfig);
+
+
 export const auth = getAuth(app);
+
+export const db = getFirestore(app);
