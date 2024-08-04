10fb35734d7a7c824d781c59e9960ef996c6248f
mahi0604 <141184262+mahi0604@users.noreply.github.com>
2024-08-04 14:53:00 -0600

    Create meal-ideas.js

---
+++ app/week-10/shopping-list/meal-ideas.js
@@ -0,0 +1,40 @@
+
+"use client";
+import { useState, useEffect } from 'react';
+
+const MealIdeas = ({ ingredient }) => {
+  const [meals, setMeals] = useState([]);
+
+  const fetchMealIdeas = async (ingredient) => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+    const data = await response.json();
+    return data.meals;
+  };
+
+  const loadMealIdeas = async () => {
+    const meals = await fetchMealIdeas(ingredient);
+    setMeals(meals);
+  };
+
+  useEffect(() => {
+    if (ingredient) {
+      loadMealIdeas();
+    }
+  }, [ingredient]);
+
+  return (
+    <div>
+      <h2>Meal Ideas</h2>
+      <ul>
+        {meals.map(meal => (
+          <li key={meal.idMeal}>
+            <img src={meal.strMealThumb} alt={meal.strMeal} style={{ width: '100px', height: '100px' }} />
+            {meal.strMeal}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MealIdeas;
